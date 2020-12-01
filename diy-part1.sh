3776ab7b2a87cbbffe67300ce0bb6db219147b7c
bihcou1 <59827513+bihcou1@users.noreply.github.com>
2020-12-01 20:32:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,10 +9,10 @@
 #=============================================================
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato package/theme
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/theme-dark
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/theme1
