f22baa71068f59b25b45ba9b6479d1585cd4d4b5
bihcou1 <59827513+bihcou1@users.noreply.github.com>
2021-11-04 17:57:39 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomato package/theme
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly-dark package/theme-dark
 git clone https://github.com/Leo-Jo-My/luci-theme-Butterfly package/theme1
-git clone https://github.com/linkease/istore/tree/main/luci/luci-app-store package/istore 
+git clone https://github.com/linkease/istore/ package/istore 
